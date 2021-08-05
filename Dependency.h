ce37d1a361c75a7c4bc56dcee340ba4d5f4cbbf4
César Gonzalo Carpio Paiva <ccarpiop@unsa.edu.pe>
2021-08-05 07:48:55 -0500

    Add files via upload

---
+++ Dependency.h
@@ -0,0 +1,16 @@
+/*
+	This projetc have a lot of dependency whit Allegro library
+	so we ordened everyone here, allegro its a basic game library
+	and we are taking advantage from the graphic interface to make
+	a simple game in 2D
+*/
+
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_font.h>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+
---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include "Dependency.h"
+#include "Level.h"
+
+
+
+
+
+
+int main(void) {
+
+    // inicializamos las librerías utilizadas
+    al_init();
+    al_init_font_addon();
+    al_init_ttf_addon();
+    al_init_image_addon();
+    al_install_keyboard();
+    al_install_mouse();
+
+
+
+    ALLEGRO_DISPLAY* display = al_create_display(800, 600);
+    ALLEGRO_TIMER* timer = NULL;
+
+
+    al_set_window_title(display, "Pandemic Hero");
+    sistema.img = al_load_bitmap("sources/image/fondo2.jpg");
+    sistema.fondo = al_map_rgb(255, 255, 255);
+    sistema.FPS = 240;
+
+    timer = al_create_timer(1.0 / sistema.FPS);
+
+
+    // creo lista de eventos
+
+    sistema.Mis_eventos = al_create_event_queue();
+
+    // asigno eventos a la lista de eventos
+
+    al_register_event_source(sistema.Mis_eventos, al_get_keyboard_event_source());
+    al_register_event_source(sistema.Mis_eventos, al_get_mouse_event_source());
+    al_register_event_source(sistema.Mis_eventos, al_get_display_event_source(display));
+    al_register_event_source(sistema.Mis_eventos, al_get_timer_event_source(timer));
+    al_start_timer(timer);
+    
+    menu();
+
+    al_destroy_display(display);
+}
